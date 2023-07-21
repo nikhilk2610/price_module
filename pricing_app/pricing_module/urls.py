6e499b019e63894b32491a6f653192e420073656
Nikhil Kumar <nikhil.kumar1@housing.com>
2023-07-21 12:18:27 +0530

    Implemented the CRUD APIs for PricingConfig along with total_price config with proper authentication

---
+++ pricing_app/pricing_module/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from . import views
+
+app_name = 'pricing_module'
+
+urlpatterns = [
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('pricing_config', views.PricingConfigListCreateView.as_view(), name='price_config_list_create'),
+    path('pricing_config/<int:pk>', views.PricingConfigRetrieveUpdateDeleteView.as_view(), name='price_config_retrieve_update_delete'),
+    path('calculate_price', views.CalculatePricingView.as_view(), name='calculate_price'),
+]
